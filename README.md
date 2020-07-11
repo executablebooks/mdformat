76a6397f090d20162abeb020491f0ade52f1f58f
Taneli Hukkinen <hukkinj1@users.noreply.github.com>
2020-07-11 20:27:21 +0300

    Add simple initial CLI handling

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # mdformat
 <!--- Don't edit the version line below manually. Let bump2version do it for you. -->
 > Version 0.0.0
+
+**NOTE:** This pre-alpha software that should not be used for anything yet.
--- mdformat/__main__.py
+++ mdformat/__main__.py
@@ -1,5 +1,35 @@
+import argparse
+from pathlib import Path
+import sys
+
+
 def run_cli() -> None:
-    print("TODO")
+    parser = argparse.ArgumentParser(
+        description="CommonMark compliant Markdown formatter"
+    )
+    parser.add_argument("paths", type=Path, nargs="+", help="Files to format")
+    parser.add_argument("--check", action="store_true")
+    args = parser.parse_args()
+
+    for path in args.paths:
+        if not path.is_file():
+            print(f'Error: File "{path}" does not exist.')
+            sys.exit(1)
+
+    for path in args.paths:
+        with path.open() as f:
+            original_str = f.read()
+
+        # TODO: do formatting here
+        formatted_str = original_str
+
+        if args.check:
+            if formatted_str != original_str:
+                print(f'Error: File "{path}" is not formatted.')
+                sys.exit(1)
+        else:
+            with path.open(mode="w") as f:
+                f.write(formatted_str)
 
 
 if __name__ == "__main__":
