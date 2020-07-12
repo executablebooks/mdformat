b6bc88c41a1a8a961b1f3341df6293672950ff94
Taneli Hukkinen <hukkinj1@users.noreply.github.com>
2020-07-12 19:11:56 +0300

    run black

--- mdformat/_util.py
+++ mdformat/_util.py
@@ -1,16 +1,17 @@
 import re
+
 from markdown_it import MarkdownIt
 
 
 def is_md_equal(md1: str, md2: str) -> bool:
     """Check if two Markdown produce the same HTML.
 
-    Renders HTML from both Markdown strings, strips whitespace and checks
-    equality. Note that this is not a perfect solution, as there can be meaningful whitespace in
-    HTML, e.g. in a <code> block.
+    Renders HTML from both Markdown strings, strips whitespace and
+    checks equality. Note that this is not a perfect solution, as there
+    can be meaningful whitespace in HTML, e.g. in a <code> block.
     """
     html1 = MarkdownIt().render(md1)
     html2 = MarkdownIt().render(md2)
-    html1 = re.sub(r'\s+', '', html1)
-    html2 = re.sub(r'\s+', '', html2)
+    html1 = re.sub(r"\s+", "", html1)
+    html2 = re.sub(r"\s+", "", html2)
     return html1 == html2
