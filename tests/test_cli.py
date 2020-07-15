99f8fc1ec43aa7a1b96b2d8446a77111dbc61195
Taneli Hukkinen <hukkinj1@users.noreply.github.com>
2020-07-15 16:11:57 +0300

    Add a test for formatting with cli

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,3 +3,13 @@ from mdformat._cli import run
 
 def test_no_files_passed():
     assert run(()) == 0
+
+
+def test_format(tmp_path):
+    unformatted_markdown = "\n\n# A header\n\n"
+    formatted_markdown = "# A header\n"
+
+    file_path = tmp_path / "test_markdown.md"
+    file_path.write_text(unformatted_markdown)
+    assert run((str(file_path),)) == 0
+    assert file_path.read_text() == formatted_markdown
