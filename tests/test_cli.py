32bb8d833d50dc8c8b8b6cf2e05ad34613d3e096
Taneli Hukkinen <hukkinj1@users.noreply.github.com>
2020-07-15 16:23:07 +0300

    Add more CLI tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,15 +1,31 @@
 from mdformat._cli import run
 
+UNFORMATTED_MARKDOWN = "\n\n# A header\n\n"
+FORMATTED_MARKDOWN = "# A header\n"
+
 
 def test_no_files_passed():
     assert run(()) == 0
 
 
 def test_format(tmp_path):
-    unformatted_markdown = "\n\n# A header\n\n"
-    formatted_markdown = "# A header\n"
-
     file_path = tmp_path / "test_markdown.md"
-    file_path.write_text(unformatted_markdown)
+    file_path.write_text(UNFORMATTED_MARKDOWN)
     assert run((str(file_path),)) == 0
-    assert file_path.read_text() == formatted_markdown
+    assert file_path.read_text() == FORMATTED_MARKDOWN
+
+
+def test_invalid_file():
+    assert run(("this is not a valid filepath?`=|><@{[]\\/,.%¤#'",)) == 1
+
+
+def test_check(tmp_path):
+    file_path = tmp_path / "test_markdown.md"
+    file_path.write_text(FORMATTED_MARKDOWN)
+    assert run((str(file_path), "--check")) == 0
+
+
+def test_check__fail(tmp_path):
+    file_path = tmp_path / "test_markdown.md"
+    file_path.write_text(UNFORMATTED_MARKDOWN)
+    assert run((str(file_path), "--check")) == 1
