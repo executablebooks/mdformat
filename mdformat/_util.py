6ac5580b01b9ef83b0b7550fd94144421f1d7f2f
Taneli Hukkinen <hukkinj1@users.noreply.github.com>
2020-10-01 15:10:35 +0300

    Undo equality check whitespace preservation

--- mdformat/_util.py
+++ mdformat/_util.py
@@ -16,10 +16,9 @@ def is_md_equal(
 ) -> bool:
     """Check if two Markdown produce the same HTML.
 
-    Renders HTML from both Markdown strings, reduces whitespace
-    sequences to single space chars, and checks equality. Note that this
-    is not a perfect solution, as there can be meaningful whitespace in
-    HTML, e.g. in a <code> block.
+    Renders HTML from both Markdown strings, strips whitespace and
+    checks equality. Note that this is not a perfect solution, as there
+    can be meaningful whitespace in HTML, e.g. in a <code> block.
     """
     html_texts = {}
     mdit = MarkdownIt()
@@ -28,9 +27,9 @@ def is_md_equal(
         mdformat.plugins.PARSER_EXTENSIONS[extension].update_mdit(mdit)
     for key, text in [("md1", md1), ("md2", md2)]:
         html = mdit.render(text)
-        html = re.sub(r"\s+", " ", html)
         for codeclass in codeformatters:
             html = re.sub(f'<code class="language-{codeclass}">.*</code>', "", html)
+        html = re.sub(r"\s+", "", html)
         html_texts[key] = html
 
     return html_texts["md1"] == html_texts["md2"]
