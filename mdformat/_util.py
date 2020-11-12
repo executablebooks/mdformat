9ef41438a011ac207c0a2b2b180a857c4fb4579f
Taneli Hukkinen <hukkinj1@users.noreply.github.com>
2020-11-12 17:52:20 +0200

    Never add same parser extension twice

--- mdformat/_util.py
+++ mdformat/_util.py
@@ -1,9 +1,39 @@
 import re
-from typing import Any, Iterable, Mapping
+from typing import Any, Iterable, Mapping, Optional
 
 from markdown_it import MarkdownIt
+from markdown_it.renderer import RendererHTML
 
 import mdformat.plugins
+
+
+def build_mdit(
+    renderer_cls: Any,
+    *,
+    mdformat_opts: Optional[Mapping[str, Any]] = None,
+    extensions: Iterable[str] = (),
+    codeformatters: Iterable[str] = (),
+) -> MarkdownIt:
+    if mdformat_opts is None:
+        mdformat_opts = {}
+
+    mdit = MarkdownIt(renderer_cls=renderer_cls)
+    mdit.options["mdformat"] = mdformat_opts
+    # store reference labels in link/image tokens
+    mdit.options["store_labels"] = True
+
+    mdit.options["parser_extension"] = []
+    for name in extensions:
+        plugin = mdformat.plugins.PARSER_EXTENSIONS[name]
+        if plugin not in mdit.options["parser_extension"]:
+            mdit.options["parser_extension"].append(plugin)
+            plugin.update_mdit(mdit)
+
+    mdit.options["codeformatters"] = {
+        lang: mdformat.plugins.CODEFORMATTERS[lang] for lang in codeformatters
+    }
+
+    return mdit
 
 
 def is_md_equal(
@@ -21,10 +51,7 @@ def is_md_equal(
     can be meaningful whitespace in HTML, e.g. in a <code> block.
     """
     html_texts = {}
-    mdit = MarkdownIt()
-    mdit.options["mdformat"] = options
-    for extension in extensions:
-        mdformat.plugins.PARSER_EXTENSIONS[extension].update_mdit(mdit)
+    mdit = build_mdit(RendererHTML, mdformat_opts=options, extensions=extensions)
     for key, text in [("md1", md1), ("md2", md2)]:
         html = mdit.render(text)
         for codeclass in codeformatters:
