e7450e8f1530a6b6e962555cc737d168492b652c
Taneli Hukkinen <hukkinj1@users.noreply.github.com>
2021-01-02 01:03:52 +0200

    Make test_cli_options more focused

--- tests/test_plugins.py
+++ tests/test_plugins.py
@@ -2,7 +2,7 @@ import argparse
 import json
 from textwrap import dedent
 from typing import Any, Mapping, Optional, Sequence, Tuple
-from unittest.mock import call, patch
+from unittest.mock import patch
 
 from markdown_it import MarkdownIt
 from markdown_it.token import Token
@@ -11,7 +11,6 @@ import mdformat
 from mdformat._cli import run
 from mdformat.plugins import CODEFORMATTERS, PARSER_EXTENSIONS
 from mdformat.renderer import MARKERS, MDRenderer
-from mdformat.renderer._util import CONSECUTIVE_KEY
 
 
 def example_formatter(code, info):
@@ -148,39 +147,22 @@ class ExamplePluginWithCli:
 
 
 def test_cli_options(monkeypatch, tmp_path):
-    """Test that -o arguments are correctly added to the options dict."""
+    """Test that CLI arguments added by plugins are correctly added to the
+    options dict."""
     monkeypatch.setitem(PARSER_EXTENSIONS, "table", ExamplePluginWithCli)
     file_path = tmp_path / "test_markdown.md"
     file_path.touch()
 
-    with patch.object(MDRenderer, "render", return_value="") as mock_method:
+    with patch.object(MDRenderer, "render", return_value="") as mock_render:
         assert run((str(file_path), "--o1", "other", "--o3", "4")) == 0
 
-    calls = mock_method.call_args_list
-    assert len(calls) == 1, calls
-    expected = {
-        "maxNesting": 20,
-        "html": True,
-        "linkify": False,
-        "typographer": False,
-        "quotes": "“”‘’",
-        "xhtmlOut": True,
-        "breaks": False,
-        "langPrefix": "language-",
-        "highlight": None,
-        "store_labels": True,
-        "parser_extension": [ExamplePluginWithCli],
-        "codeformatters": {},
-        "mdformat": {
-            "arg_name": 4,
-            "check": False,
-            CONSECUTIVE_KEY: False,
-            "o1": "other",
-            "o2": "a",
-            "paths": [str(file_path)],
-        },
-    }
-    assert calls[0] == call([], expected, {}), calls[0]
+    (call_,) = mock_render.call_args_list
+    posargs = call_[0]
+    # Options is the second positional arg of MDRender.render
+    opts = posargs[1]
+    assert opts["mdformat"]["o1"] == "other"
+    assert opts["mdformat"]["o2"] == "a"
+    assert opts["mdformat"]["arg_name"] == 4
 
 
 class ExampleASTChangingPlugin:
