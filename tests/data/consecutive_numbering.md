3fbe739ce5f5ba00ee9aab9f51a5065e305fc377
Taneli Hukkinen <hukkinj1@users.noreply.github.com>
2021-01-04 01:53:31 +0200

    Improve tests

---
+++ tests/data/consecutive_numbering.md
@@ -0,0 +1,40 @@
+numbered lists [consecutive]
+.
+1. a
+2. b
+3. c
+   d
+.
+1. a
+2. b
+3. c
+   d
+.
+
+numbered lists starting number [consecutive]
+.
+099. a
+100. b
+101. c
+     d
+.
+099. a
+100. b
+101. c
+     d
+.
+
+numbered lists nested [consecutive]
+.
+1. a
+2. b
+   1. x
+   2. y
+      z
+.
+1. a
+2. b
+   1. x
+   2. y
+      z
+.
---
+++ tests/test_style.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+from markdown_it.utils import read_fixture_file
+import pytest
+
+import mdformat
+from mdformat.renderer._util import CONSECUTIVE_KEY
+
+
+@pytest.mark.parametrize(
+    "fixture_file,options",
+    [("default_style.md", {}), ("consecutive_numbering.md", {CONSECUTIVE_KEY: True})],
+)
+def test_style(fixture_file, options):
+    """Test Markdown renderer renders expected style."""
+    cases = read_fixture_file(Path(__file__).parent / "data" / fixture_file)
+    for case in cases:
+        line, title, text, expected = case
+        md_new = mdformat.text(text, options=options)
+        if not md_new == expected:
+            print(md_new)
+        assert md_new == expected
