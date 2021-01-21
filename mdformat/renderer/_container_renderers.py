c2e30a13091ce2c5d3f5ffb8f9bfed544a147127
Taneli Hukkinen <hukkinj1@users.noreply.github.com>
2021-01-21 06:20:50 +0200

    Only escape line starting "-" or "+" if followed by space or tab

--- mdformat/renderer/_container_renderers.py
+++ mdformat/renderer/_container_renderers.py
@@ -153,11 +153,9 @@ def paragraph_close(
     # (otherwise it will be interpreted as an ATX heading).
     lines = [f"\\{line}" if line.startswith("#") else line for line in lines]
     # Make sure a paragraph line does not start with "-" or "+"
+    # followed by a space, tab, or end of line.
     # (otherwise it will be interpreted as list item).
-    lines = [
-        f"\\{line}" if (line.startswith("-") or line.startswith("+")) else line
-        for line in lines
-    ]
+    lines = [f"\\{line}" if re.match(r"[-+]( |\t|$)", line) else line for line in lines]
     # If a line starts with a number followed by "." or ")" followed by
     # whitespace or end of line, escape the "." or ")" or it will be
     # interpreted as ordered list item.
