2bd5c96b66d567e956852b77c9833381c7d9f625
Taneli Hukkinen <hukkinj1@users.noreply.github.com>
2021-01-22 04:09:50 +0200

    Bump version: 0.5.5 → 0.5.6

--- mdformat/__init__.py
+++ mdformat/__init__.py
@@ -1,4 +1,4 @@
 __all__ = ("file", "text")
-__version__ = "0.5.5"  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
+__version__ = "0.5.6"  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
 
 from mdformat._api import file, text
