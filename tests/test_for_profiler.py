daf4538b2fb8b62fc9c59b042f37767395b3d10f
Taneli Hukkinen <hukkinj1@users.noreply.github.com>
2021-03-28 00:33:02 +0200

    TESTS: Add a test useful for profiling

---
+++ tests/test_for_profiler.py
@@ -0,0 +1,25 @@
+"""Test for profiling.
+
+This test can be useful for profiling, as most of the execution time
+will be spent parsing and rendering instead of managing pytest execution
+environment. The test simply ensures that the README and Markdown docs
+in this project are formatted. To get profiler results:
+  - `pip install pytest-profiling`
+  - `pytest -k test_for_profiler --profile-svg`
+"""
+from pathlib import Path
+
+from mdformat._cli import run
+
+PROJECT_ROOT = Path(__file__).parent.parent
+# Make a few asserts to ensure this actually is the project root
+# (a safeguard against refactorings where this file is moved).
+assert (PROJECT_ROOT / "docs").exists()
+assert (PROJECT_ROOT / "README.md").exists()
+assert (PROJECT_ROOT / "mdformat").exists()
+
+
+def test_for_profiler():
+    docs_path = PROJECT_ROOT / "docs"
+    readme_path = PROJECT_ROOT / "README.md"
+    assert run([str(docs_path), str(readme_path), "--check"]) == 0
