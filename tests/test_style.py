e14c73abdaece47d3c9f68205c6507235881fa42
Taneli Hukkinen <3275109+hukkin@users.noreply.github.com>
2021-08-24 14:49:14 +0300

    FIX: Dont error when an autolink contains spaces

--- tests/test_style.py
+++ tests/test_style.py
@@ -4,23 +4,27 @@ from markdown_it.utils import read_fixture_file
 import pytest
 
 import mdformat
-from mdformat.renderer._util import CONSECUTIVE_KEY
+import mdformat._cli
 
 
 @pytest.mark.parametrize(
     "fixture_file,options",
     [
-        ("default_style.md", {}),
-        ("consecutive_numbering.md", {CONSECUTIVE_KEY: True}),
-        ("wrap_width_50.md", {"wrap": 50}),
+        ("default_style.md", []),
+        ("consecutive_numbering.md", ["--number"]),
+        ("wrap_width_50.md", ["--wrap=50"]),
     ],
 )
-def test_style(fixture_file, options):
+def test_style(fixture_file, options, tmp_path):
     """Test Markdown renderer renders expected style."""
+    file_path = tmp_path / "test_markdown.md"
     cases = read_fixture_file(Path(__file__).parent / "data" / fixture_file)
     for case in cases:
         line, title, text, expected = case
-        md_new = mdformat.text(text, options=options)
+        file_path.write_text(text)
+        assert mdformat._cli.run([str(file_path), *options]) == 0
+        md_new = file_path.read_text()
         if md_new != expected:
+            print("Formatted (unexpected) Markdown below:")
             print(md_new)
         assert md_new == expected
