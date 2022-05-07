038f54ba1f870bc2359f8a96053d907b3ba551dc
Taneli Hukkinen <3275109+hukkin@users.noreply.github.com>
2022-05-08 00:54:45 +0300

    Absolufy imports (#321)
    * Absolufy imports
    
    * Update doc deps

--- src/mdformat/codepoints/__init__.py
+++ src/mdformat/codepoints/__init__.py
@@ -5,8 +5,8 @@ __all__ = (
     "ASCII_WHITESPACE",
 )
 
-from ._unicode_punctuation import UNICODE_PUNCTUATION
-from ._unicode_whitespace import UNICODE_WHITESPACE
+from mdformat.codepoints._unicode_punctuation import UNICODE_PUNCTUATION
+from mdformat.codepoints._unicode_whitespace import UNICODE_WHITESPACE
 
 ASCII_CTRL = frozenset(chr(i) for i in range(32))
 ASCII_WHITESPACE = frozenset({chr(9), chr(10), chr(11), chr(12), chr(13), chr(32)})
