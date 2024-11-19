c22a0b845fd833e22ecb500a6f11020c299993fe
Taneli Hukkinen <3275109+hukkin@users.noreply.github.com>
2024-11-19 21:52:11 +0200

    deprecate: `mdformat.codepoints.ASCII_WHITESPACE` (#481)

--- src/mdformat/codepoints/__init__.py
+++ src/mdformat/codepoints/__init__.py
@@ -5,8 +5,27 @@ __all__ = (
     "ASCII_WHITESPACE",
 )
 
+import warnings
+
 from mdformat.codepoints._unicode_punctuation import UNICODE_PUNCTUATION
 from mdformat.codepoints._unicode_whitespace import UNICODE_WHITESPACE
 
 ASCII_CTRL = frozenset(chr(i) for i in range(32))
-ASCII_WHITESPACE = frozenset({chr(9), chr(10), chr(11), chr(12), chr(13), chr(32)})
+
+
+def __getattr__(name: str) -> frozenset[str]:
+    """Attribute getter fallback.
+
+    Used during the deprecation period of `ASCII_WHITESPACE`.
+    """
+    if name == "ASCII_WHITESPACE":
+        warnings.warn(
+            "ASCII_WHITESPACE is deprecated because CommonMark v0.30 no longer "
+            "defines ASCII whitespace.",
+            DeprecationWarning,
+            stacklevel=2,
+        )
+        return frozenset({chr(9), chr(10), chr(11), chr(12), chr(13), chr(32)})
+    raise AttributeError(
+        f"module {__name__!r} has no attribute {name!r}"
+    )  # pragma: no cover
